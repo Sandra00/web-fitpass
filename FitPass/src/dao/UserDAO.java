b081e69c426b7a56475ce3c39879d02497848acb
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-05-25 21:21:26 +0200

    Add basic backend functionality

---
+++ FitPass/src/dao/UserDAO.java
@@ -0,0 +1,47 @@
+package dao;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import beans.User;
+
+public class UserDAO {
+	private List<User> users;
+	
+	public UserDAO(String path) {
+		users = new ArrayList<User>();
+		loadUsers(path);
+	}
+	
+	
+	public User findUserByUsername(String username) {
+		for(User user : users) {
+			if(user.getUsername().equals(username)) {
+				return user;
+			}
+		}
+		return null;
+	}
+	
+	private void loadUsers(String path) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			users = Arrays.asList(mapper.readValue(Paths.get(path + "users.txt").toFile(), User[].class));
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ FitPass/src/services/UserService.java
@@ -0,0 +1,57 @@
+package services;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import beans.User;
+import dao.UserDAO;
+
+@Path("")
+public class UserService {
+	
+	@Context
+	ServletContext ctx;
+	
+	public UserService() {
+		
+	}
+	
+	@PostConstruct
+	private void init() {
+		if (ctx.getAttribute("userDAO") == null) {
+	    	String contextPath = ctx.getRealPath("");
+			ctx.setAttribute("userDAO", new UserDAO(contextPath));
+		}
+	}
+	
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response login(User user, @Context HttpServletRequest request) {
+		UserDAO userDao = (UserDAO) ctx.getAttribute("userDAO");
+		User loggedUser = userDao.findUserByUsername(user.getUsername());
+		if (loggedUser == null) {
+			return Response.status(400).entity("Invalid username and/or password").build();
+		}
+		request.getSession().setAttribute("user", loggedUser);
+		return Response.status(200).build();
+	}
+	
+	@GET
+	@Path("/currentUser")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User login(@Context HttpServletRequest request) {
+		return (User) request.getSession().getAttribute("user");
+	}
+}
