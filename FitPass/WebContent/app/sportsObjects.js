bdb71c1d09fa02c8de0f1a3689f490d221737894
Sandra Brkic <sandra.brkic5@gmail.com>
2022-06-02 16:33:11 +0200

    Implement search by name

--- FitPass/WebContent/app/sportsObjects.js
+++ FitPass/WebContent/app/sportsObjects.js
@@ -1,9 +1,27 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		sportsObjects: null
+		sportsObjects: [],
+		nameSearch:'',
+		typeSearch:'',
+		locationSearch:'',
+		gradeSearch:''
 	},
 	mounted(){
+		//axios.get('rest/objects')
+		//.then((response) => {
+			//this.sportsObjects = response.data;
+			
+			// sorting sports objects so that the active ones are displayed first
+			//this.sportsObjects.sort((x, y) => { return (x.status === y.status)? 0 : x.status? -1 : 1; });
+		//});
+		
+	},
+	methods: {
+		
+		
+	},
+	created(){
 		axios.get('rest/objects')
 		.then((response) => {
 			this.sportsObjects = response.data;
@@ -11,11 +29,13 @@ var app = new Vue({
 			// sorting sports objects so that the active ones are displayed first
 			this.sportsObjects.sort((x, y) => { return (x.status === y.status)? 0 : x.status? -1 : 1; });
 		});
-		
 	},
-	methods: {
-		
-		
+	computed:{
+		filteredSportsObjects:function(){
+			return this.sportsObjects.filter((sportsObject) => {
+				return sportsObject.name.match(this.nameSearch);
+			})
+		}
 	}
 	
 });
