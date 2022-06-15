11ca88fe03d0b37c2cdc9cf68084f1821ebfb420
Sandra Brkic <sandra.brkic5@gmail.com>
2022-06-15 07:57:05 +0200

    Adding new object works without times

--- FitPass/src/dao/SportsObjectDAO.java
+++ FitPass/src/dao/SportsObjectDAO.java
@@ -75,6 +75,7 @@ public class SportsObjectDAO {
 		return false;
 	}
 	public boolean newSportObject(SportsObject sportsObject) {
+		System.out.println(exists(sportsObject));
 		if(exists(sportsObject)) return false;
 		sportsObjects.add(sportsObject);
 		saveSportsObjects();
