20ea095fff8d1594c40d45f03b35e98b8857894f
Sandra Brkic <sandra.brkic5@gmail.com>
2022-06-17 10:01:03 +0200

    Add reading from path

--- FitPass/WebContent/app/edit_content.js
+++ FitPass/WebContent/app/edit_content.js
@@ -1,12 +1,25 @@
 var app = new Vue({
 	el: '#app',
 	data:{
-		content: null
+		content: null,
+		label:null
 	},
 	mounted(){
 		var location = window.location.href.toString();
 		var params = location.split("?");
 		var value = params[1].split("=")[1];
 		value = value.replace('%20', ' ');
+		this.label = value.toString();
+		axios.get(
+			'rest/objects/currentContent',
+			{
+				params: {
+					name: this.label
+				}
+			}
+		)
+		.then((response) => {
+			this.content = response.data;
+		})
 	}
 })
