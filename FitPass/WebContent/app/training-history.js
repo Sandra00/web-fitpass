e38390aaca256168c39e66ca7c5e6c4907d8fb1b
Sandra Brkic <sandra.brkic5@gmail.com>
2022-06-21 08:10:33 +0200

    Find customers training history

--- FitPass/WebContent/app/training-history.js
+++ FitPass/WebContent/app/training-history.js
@@ -1,9 +1,24 @@
 const vm = new Vue({
 	el: '#app',
 	data: {
-		customersTrainingHistroy: null
+		customersTrainingHistory: null,
+		currentUser = null
 	},
 	mounted(){
-		
+		axios.get('rest/currentUser')
+		.then((response) => {
+			this.currentUser = response.data
+		})
+		xios.get(
+			'rest/training-history/find-customer-training-history',
+			{
+				params: {
+					user: this.currentUser
+				}
+			}
+		)
+		.then((response) => {
+			this.customersTraininghistory = response.data;
+		})
 	}
 })
