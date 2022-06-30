72b9c5d47e522ad4ee35d64e0bffd9a0989e9925
Sandra Brkic <sandra.brkic5@gmail.com>
2022-06-30 08:33:55 +0200

    Add search without dates

--- FitPass/WebContent/app/coach-trainigns.js
+++ FitPass/WebContent/app/coach-trainigns.js
@@ -3,9 +3,22 @@ const vm = new Vue({
 	data: {
 		currentUsername: null,
 		coachTrainings: [],
-		trainingsMap: []
+		trainingsMap: [],
+		nameSportObjectSearch: '',
+		startPriceSearch: null,
+		endPriceSearch: null,
+		startDateSearch: null,
+		endDateSearch: null,
+		startDateSearch: null,
+		endDateSearch: null,
+		sportObjectTypeFilter: "all",
+		trainingTypeFilter: "all",
+		sortIndex: null,
+		sportsObject: null,
+		training: null,
+		price: null
 	},
-	mounted(){
+	created(){
 		axios.get('rest/currentUser')
 			.then((response) => {
 				this.currentUsername = response.data.username;
@@ -66,5 +79,97 @@ const vm = new Vue({
 					)
 			window.location.reload()
 		}
+	},
+	computed:{
+		filteredCoachTrainings:function(){
+			
+			return this.coachTrainings.filter((trainingH) => {
+				let keep = true;
+				let maxPrice = 10000;
+					if(this.endPriceSearch != null || this.endPriceSearch == ""){
+						maxPrice = this.endPriceSearch;
+					}
+					if(maxPrice == ""){
+						maxPrice = 10000;
+					}
+					
+					for(let i = 0; i < this.trainingsMap.length; ++i){
+						if(this.trainingsMap[i].id == trainingH.trainingId){
+							this.training = this.trainingsMap[i].training;
+							this.price = this.trainingsMap[i].price;
+							keep = keep && this.training.sportsObject.toLowerCase().match(this.nameSportObjectSearch.toLowerCase())
+								 && (Number(this.training.price) >= Number(this.startPriceSearch))
+								 && (Number(this.training.price) <= maxPrice);
+							break;
+						}
+					}
+					/*
+				axios.get(
+						'rest/objects/training',
+						{
+							params:{
+								trainingId: trainingH.trainingId
+							}
+						}
+					)
+					.then((response) => {
+						this.training = response.data;
+						this.sportsObject = response.data.sportsObject;
+						this.price = response.data.price;
+						//keep = keep && (this.training.sportsObject.toLowerCase().match(this.nameSportObjectSearch.toLowerCase()))
+						//return keep;
+					})
+					*/
+					//keep = keep;
+					//keep = keep && this.training.sportsObject.toLowerCase().match(this.nameSportObjectSearch.toLowerCase())
+					//	    && (Number(this.training.price) >= Number(this.startPriceSearch));
+					
+					//if(this.sportsObject != null && this.price != null){
+					//keep = keep && this.sportsObject.toLowerCase().match(this.nameSportObjectSearch.toLowerCase())
+						    //&& (Number(this.price) >= Number(this.startPriceSearch));
+							//&& (Number(this.training.price) <= maxPrice); //training.price.toString().match(this.startPriceSearch);
+					//}
+					
+				return keep;
+			})
+			
+		}
 	}
+	/*
+	computed:{
+		filteredCoachTrainings:function(){
+			
+			return this.coachTrainings.filter((trainingHistory) => {
+				let keep = true;
+				axios.get(
+						'rest/objects/training',
+						{
+							params:{
+								trainingId: trainingHistory.trainingId
+							}
+						}
+					)
+					.then((response) => {
+						this.training = response.data;
+						this.sportsObject = response.data.sportsObject;
+						//keep = keep && this.training.sportsObject.toLowerCase().match(this.nameSportObjectSearch.toLowerCase())
+					})
+					let maxPrice = 10000;
+					if(this.endPriceSearch != null || this.endPriceSearch == ""){
+						maxPrice = this.endPriceSearch;
+					}
+					if(maxPrice == ""){
+						maxPrice = 10000;
+					}
+					if(this.training != null){
+						keep = keep && this.training.sportsObject.toLowerCase().match(this.nameSportObjectSearch.toLowerCase())
+						    && (Number(this.training.price) >= Number(this.startPriceSearch));
+							//&& (Number(this.training.price) <= maxPrice); //training.price.toString().match(this.startPriceSearch);
+					}
+				return keep;
+			})
+			
+		}
+	}
+	*/
 })
