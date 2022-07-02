dbd1e99aff387865d06829bbf434f62615a0ed70
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-07-02 18:42:44 +0200

    Add search and filter features

--- FitPass/WebContent/app/admin-panel.js
+++ FitPass/WebContent/app/admin-panel.js
@@ -2,9 +2,14 @@ const vm = new Vue ({
 	el: '#app',
 	data: {
 		allUsers: [],
-		promoCodes: []
+		promoCodes: [],
+		usernameSearch: '',
+		firstNameSearch: '',
+		lastNameSearch: '',
+		roleFilter: '',
+		typeFilter: ''
 	},
-	mounted(){
+	created(){
 		axios.get('rest/admin/all-users')
 		.then((response) => {
 			this.allUsers= response.data;
@@ -16,23 +21,47 @@ const vm = new Vue ({
 			this.promoCodes.forEach(async item => {
 				item.expirationDate = await this.formatDateTime(item.expirationDate);
 			});
-		});
-		
-		
+		});	
 	},
 	methods: {
 		
-		
-		formatDateTime(date){
-		if(date[3] < 10){
-			date[3] = '0' + date[3];
-		}
-		if(date[4] < 10){
-			date[4] = '0' + date[4];
-		}
+		/*
+		filterType(type){
+			axios.get('rest/admin/all-users')
+			.then((response) => {
+				this.allUsers = response.data;
+				this.allUsers.filter((allUsers)=>{
+					return allUsers.userType.match(type);
+				});
+			});
+		},
+		*/
+		formatDateTime(date) {
+			if(date[3] < 10){
+				date[3] = '0' + date[3];
+			}
+			if(date[4] < 10){
+				date[4] = '0' + date[4];
+			}
 			return date[2] + '/' + date[1] + '/' + date[0] + ' ' + date[3] + ':' + date[4];
 		}
 		
 		
+	},
+
+	computed: {
+
+		filteredUsers: function(){
+			return this.allUsers.filter((user) => {					
+				let show = true;
+				show &= user.username.toLowerCase().search(this.usernameSearch.toLowerCase()) >= 0;
+				show &= user.name.toLowerCase().search(this.firstNameSearch.toLowerCase()) >= 0;
+				show &= user.surname.toLowerCase().search(this.lastNameSearch.toLowerCase()) >= 0;
+				show &= user.userType.search(this.roleFilter) >= 0 || this.roleFilter == 'ALL' || this.roleFilter == '';
+				//show &= user.lastName.toLowerCase().search(this.lastNameSearch.toLowerCase()) >= 0;
+				
+				return show;
+			});
+		}
 	}
 });
