e06ce5484b50d6940e41dc444764ce36737053cb
Sandra Brkic <sandra.brkic5@gmail.com>
2022-07-06 18:44:56 +0200

    Add maps

--- FitPass/WebContent/app/new_sportObject.js
+++ FitPass/WebContent/app/new_sportObject.js
@@ -21,6 +21,7 @@
         file: null
     },
     mounted(){
+	this.displayMap();
 		axios.get('rest/freeManagers')
 		.then((response) => {
 			this.managers = response.data;
@@ -28,7 +29,75 @@
 		})
 	},
     methods: {
-		
+	
+		displayMap: function () {
+
+            let lat = 45.2396;
+            let lon = 19.8227;
+            let map = new ol.Map({
+                layers: [
+                    new ol.layer.Tile({
+                        source: new ol.source.OSM()
+                    })
+                ],
+                view: new ol.View({
+                    center: ol.proj.fromLonLat([lon, lat]),
+                    zoom: 10
+                })
+            });
+
+            setTimeout(() => {
+                if (map) {
+                    map.setTarget("map-create");
+                    let c = document.getElementById("map-create").childNodes;
+                    c[0].style.borderRadius = '15px';
+                }
+            }, 50);
+
+            map.on('click', evt => {
+                let coord = ol.proj.toLonLat(evt.coordinate);
+                this.reverseGeocode(coord);
+                this.mapClicked = true;
+            })
+        },
+        reverseGeocode: function (coords) {
+            fetch('http://nominatim.openstreetmap.org/reverse?format=json&lon=' + coords[0] + '&lat=' + coords[1])
+                .then(function (response) {
+                    return response.json();
+                }).then(json => {
+                console.log(coords);
+                this.longitude = coords[0];
+                this.latitude = coords[1];
+                console.log(json.address);
+                if (json.address.city) {
+                    this.town = json.address.city;
+                } else if (json.address.city_district) {
+                    this.town = json.address.city_district;
+                }
+
+                if (json.address.road) {
+                    this.street = json.address.road;
+                }
+
+                if (json.address.house_number) {
+                    this.number = json.address.house_number;
+                }
+
+                if (json.address.postcode) {
+                    this.postNumber = json.address.postcode;
+                }
+
+            });
+        },
+        geocode: async function (street, city) {
+            await fetch('http://nominatim.openstreetmap.org/search?format=json&street=' + street + '&city=' + city + '&country=srbija')
+                .then(function (response) {
+                    return response.json();
+                }).then(json => {
+                    this.location.longitude = json[0].lon;
+                    this.location.latitude = json[0].lat;
+                });
+        },
         async register() {
 	
 			
@@ -101,6 +170,7 @@
         getFormValues (submitEvent) {
             this.register();
         }
+        
         
     }
 });
